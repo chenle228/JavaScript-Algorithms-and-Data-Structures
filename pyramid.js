9fbf26fd4a8ff0969460219984868242f0b393b4
chenle228 <e1156512@u.nus.edu>
2024-08-31 16:40:26 +0800

    Create a pyramid generator

--- pyramid.js
+++ pyramid.js
@@ -4,7 +4,9 @@ const rows = [];
 let inverted = true;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return " ".repeat(rowCount - rowNumber) 
+  + character.repeat(2 * rowNumber - 1) 
+  + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 1; i <= count; i++) {
